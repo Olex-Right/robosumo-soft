01f55e21440d4d93e908ce2ca69d092fb5ee6d82
Olex-Right <sanokoo.its.me@gmail.com>
2023-12-11 13:46:16 +0100

    Added right, left sensors handling

--- main_soft/main_soft.ino
+++ main_soft/main_soft.ino
@@ -1,6 +1,8 @@
 #define BUTTON_PIN 12
 #define TRIGGER 8
-#define ECHO 7
+#define FORWARD_ECHO 7
+#define LEFT_ECHO 4
+#define RIGHT_ECHO 2
 #define LEFT_BACKWARD 11
 #define LEFT_FORWARD 10
 #define RIGHT_BACKWARD 3
@@ -13,16 +15,20 @@ const int blackColor = 300;
 const int whiteColor = 700;
 const int errorRange = 50;
 
-int distance = 0;
+int forwardDistance = 0;
+int leftDistance = 0;
+int rightDistance = 0;
 bool active = false;
-short robotState = 0;  //0 - stop; 1- move Forward; 2 - move Backward; 3 - turn Right; 4 - turn Left;
+short robotState = 0; // 0 - stop; 1- move Forward; 2 - move Backward; 3 - turn Right; 4 - turn Left;
+short previousState = 3;
 
-
-void setup() 
+void setup()
 {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(ECHO, INPUT);
+  pinMode(LEFT_ECHO, INPUT);
+  pinMode(RIGHT_ECHO, INPUT);
+  pinMode(FORWARD_ECHO, INPUT);
   pinMode(TRIGGER, OUTPUT);
   pinMode(LEFT_BACKWARD, OUTPUT);
   pinMode(LEFT_FORWARD, OUTPUT);
@@ -30,96 +36,120 @@ void setup()
   pinMode(RIGHT_FORWARD, OUTPUT);
 }
 
-int sensor() 
+int sensor(int ECHO_PIN)
 {
   digitalWrite(TRIGGER, 1);
   delay(10);
   digitalWrite(TRIGGER, 0);
-  while (digitalRead(ECHO) == 0)
+  while (digitalRead(ECHO_PIN) == 0)
     ;
   // begin and time in mikro seconds
   unsigned long begin = micros();
   unsigned long time;
-  while (micros() - begin <= 11660 && digitalRead(ECHO) == 1) 
+  while (micros() - begin <= 11660 && digitalRead(ECHO_PIN) == 1)
   {
     time = micros() - begin;
   }
-  if (time >= 11660) return -1;
-  // return distance in cm; 343 - speed of sound; / 2 - sound goes forward and backward
+  if (time >= 11660)
+    return -1;
+  // return distance in cm; 343 m/sec - speed of sound; time in mikroSec (1e-6 sec * 1e4 m/sec = 10e-2m = cm);  / 2 - sound goes forward and backward
   return (343 * (time / 10000.0)) / 2;
-  return 0;
 }
 
-
-
-void wait() 
+void wait()
 {
   while (digitalRead(BUTTON_PIN) == 0)
     ;
-  //delay(5000);
+  // delay(5000);
 }
 
-
-
-void loop() 
+void loop()
 {
-  //wait for button
-  if ((digitalRead(BUTTON_PIN) == 0) && !active) 
+  delay(100);
+  // wait for button
+  if ((digitalRead(BUTTON_PIN) == 0) && !active)
   {
     wait();
     active = !active;
   }
 
-  
-  if (active) 
+  if (active)
   {
-    distance = sensor();
-    Serial.println(distance);
+    forwardDistance = sensor(FORWARD_ECHO);
+    rightDistance = sensor(RIGHT_ECHO);
+    leftDistance = sensor(LEFT_ECHO);
+    Serial.println("forwardDistance " + forwardDistance);
+    Serial.println("rightDistance " + rightDistance);
+    Serial.println("leftDistance " + leftDistance);
 
     // Read the value of the color sensor on analog pin 2
     int colorValue = analogRead(2);
 
-    if (colorValue >= (blackColor - errorRange) && colorValue <= (blackColor + errorRange)) 
+    if (colorValue >= (blackColor - errorRange) && colorValue <= (blackColor + errorRange))
     {
-        // Detected black color
-        Serial.println("Black Color Detected");
+      // Detected black color
+      Serial.println("Black Color Detected");
     }
-    else if (colorValue >= (whiteColor - errorRange) && colorValue <= (whiteColor + errorRange)) 
+    else if (colorValue >= (whiteColor - errorRange) && colorValue <= (whiteColor + errorRange))
     {
-        // Detected white color
-        Serial.println("White Color Detected");
+      // Detected white color
+      Serial.println("White Color Detected");
     }
-    else 
+    else
     {
-        // Unknown color
-        Serial.println("Unknown Color");
-        stopMovement();
-        active=false;
-        robotState = 0;
+      // Unknown color
+      Serial.println("Unknown Color");
+      stopMovement();
+      active = false;
+      robotState = 0;
     }
 
-    // less than 75 cm
-    if (distance < DISTANCE && distance!=-1 && active) 
+    // less than DISTANCE in cm
+    if (forwardDistance < DISTANCE && forwardDistance != -1 && active)
     {
-      Serial.println(2);
-      if (robotState != 1) 
+      Serial.println("MOVE Forward");
+      if (robotState != 1)
       {
+        previousState = robotState ? robotState : 3;
         stopMovement();
         moveForward();
         robotState = 1;
       }
-    } else if(active) 
+    }
+    else if(leftDistance < DISTANCE && leftDistance != -1 && active)
     {
-      if (robotState != 3) 
+      Serial.println("TURN LEFT");
+      if (robotState != 4)
+      {
+        stopMovement();
+        rotateLeft();
+        robotState = 4;
+      }
+    }
+    else if(rightDistance < DISTANCE && rightDistance != -1 && active)
+    {
+      Serial.println("TURN RIGHT");
+      if (robotState != 3)
       {
         stopMovement();
         rotateRight();
         robotState = 3;
       }
     }
-  } else 
+    else if (active)
+    {
+      Serial.println("BASIC MOVE");
+      if (robotState != 3 && robotState != 4)
+      {
+        stopMovement();
+        previousState == 3 ? rotateRight() : rotateLeft();
+        robotState = previousState;
+      }
+    }
+  }
+  else
   {
-    if (robotState != 0) 
+    if (robotState != 0)
     {
       stopMovement();
       robotState = 0;
@@ -127,9 +157,7 @@ void loop()
   }
 }
 
-
-
-void stopMovement() 
+void stopMovement()
 {
   digitalWrite(LEFT_FORWARD, LOW);
   digitalWrite(RIGHT_FORWARD, LOW);
@@ -137,25 +165,25 @@ void stopMovement()
   digitalWrite(RIGHT_BACKWARD, LOW);
 }
 
-void moveForward() 
+void moveForward()
 {
   digitalWrite(LEFT_FORWARD, HIGH);
   digitalWrite(RIGHT_FORWARD, HIGH);
 }
 
-void moveBackward() 
+void moveBackward()
 {
   digitalWrite(LEFT_BACKWARD, HIGH);
   digitalWrite(RIGHT_BACKWARD, HIGH);
 }
 
-void rotateRight() 
+void rotateRight()
 {
   analogWrite(LEFT_FORWARD, ROTATION_GAIN);
   analogWrite(RIGHT_BACKWARD, ROTATION_GAIN);
 }
 
-void rotateLeft() 
+void rotateLeft()
 {
   analogWrite(LEFT_BACKWARD, ROTATION_GAIN);
   analogWrite(RIGHT_FORWARD, ROTATION_GAIN);
